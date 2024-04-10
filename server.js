9edcd783b29a79fca8fc1210be0887a958ad886e
tylerchen14 <tylerchenpo@gmail.com>
2024-04-10 09:07:12 +0800

    修好連線異常

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const app = express()
 const server = require('http').Server(app)
-const io = require('socket.io')(server, {
-  cors: {
-    origin: ["http://localhost:3000"],
-  }
-})
+const io = require('socket.io')(server)
+
+io.origins(["*:*"]);
 
 const { v4: uuidV4 } = require('uuid')
 
@@ -13,8 +11,8 @@ app.get('/05-streaming', (req, res) => {
   res.redirect(`/05-streaming/${uuidV4()}`)
 })
 
-app.get('/:room', (req, res) => {
-  return room = req.params.room
+app.get('/05-streaming/:room', (req, res) => {
+  res.render('05-streaming', { room: req.params.room })
 })
 
 // 確認連線
@@ -28,24 +26,7 @@ io.on('connection', socket => {
     socket.join(room)
     socket.to(room).broadcast.emit('user-connect', user)
   })
-
 })
 
-server.listen(3001, () => {
-  console.log('listening on port 3001')
-})
+server.listen(3001)
 
-// const io = require('socket.io')(4020, {
-//   cors: {
-//     origin: ["http://localhost:3000"],
-//   }
-// });
-
-// // 確認連線
-// io.on('connection', socket => {
-//   console.log(socket.id);
-
-//   socket.on('sendComment', comment => {
-//     io.emit('receiveComment', comment)
-//   })
-// })
