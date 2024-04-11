feaa2f867d3f1f41e45f682cfe8ba98ace610625
tylerchen14 <tylerchenpo@gmail.com>
2024-04-11 21:48:40 +0800

    調整畫面

--- server.js
+++ server.js
@@ -9,16 +9,25 @@ const io = require('socket.io')(server, {
 
 // 確認連線
 io.on('connection', socket => {
+  console.log(`連線成功，用戶號碼為 ${socket.id}`);
 
-  socket.on('sendComment', comment => {
-    io.emit('receiveComment', comment)
+  // 聊天室
+
+  socket.on('joinRoom', room => {
+    socket.join(room)
   })
 
+  socket.on('sendComment', (comment, room) => {
+    io.to(room).emit('receiveComment', comment)
+  })
+
+  // 視訊
   socket.on('join-room', (room, id) => {
     socket.join(room)
     socket.to(room).broadcast.emit('user-connected', id)
   })
 })
 
-server.listen(3001)
-
+server.listen(3001, () => {
+  console.log(`正在連線伺服器 3001`)
+})
